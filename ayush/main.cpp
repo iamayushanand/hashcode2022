a6fee920b54215924d73723760e88457f69733a5
Ayush <iamayushanand@gmail.com>
2022-02-24 22:43:04 +0530

    added template

---
+++ ayush/main.cpp
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+ 
+using namespace std;
+const int INF= 2147483647;
+
+#define MAX(a,b) ((a>b)?a:b)
+#define MIN(a,b) ((a<b)?a:b)
+#define MOD 1000000007
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);	
+	return 0;
+}
